4126a46dbf35353ccb3f96ae506652a3d8aa086a
bobojin <boboy3@gmail.com>
2016-08-11 15:05:21 +0800

    Preparing for the new UI

--- api/display_text.php
+++ api/display_text.php
@@ -1,19 +1,19 @@
 <?php
 	
-	/*所有显示文字的编辑在这里进行*/
+// 	所有显示文字的编辑在这里进行
 	
-	/*regular*/
+// 	regular
 	$return_text = "返回";
 	
-	/*homepage*/
+// 	homepage
 	$title_hp_text = "Keyboardance - 快捷键";
 	$search_placeholder_hp_text = "输入名称后回车";
 	$search_button_hp_text = "搜索";
 	$title_content_hp_text = "=TOP5推荐=";
 	
-	/*search results page*/
+// 	search results page
 	
-	/*shortcut page*/
+// 	shortcut page
 	$print_sc_text = "获取打印版";
 	$visit_count_sc_text = "访问次数：";
 	$tag_sc_text = "标签：";
